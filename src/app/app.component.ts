9c3e1bff0d11fc26cb017c1341647f426b4d5e7a
David Manzer <david.manzere@softrams.com>
2021-01-20 10:14:49 -0700

    string interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styles: [`
+  h3 {
+    color: dodgerblue;
+  }
+  `]
 })
 export class AppComponent {
-  name = 'David';
+  // name = 'David';
 }
